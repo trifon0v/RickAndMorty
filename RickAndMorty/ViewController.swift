31316c0f6be278d1349932a9c5d278ba6eeff50b
Dmitrii Trifonov <dmitriy.trifonov@tabby.ai>
2024-12-21 08:49:50 +0100

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Дмитрий Трифонов on 21/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
